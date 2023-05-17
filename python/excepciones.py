4d0d749907b25932f7a8d166a94c78aba59bcb3e
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-17 17:50:42 -0500

    1

--- python/excepciones.py
+++ python/excepciones.py
@@ -1,31 +1,32 @@
 #Ejercicios Errores con excepciones
 #ZeroDivisiorError
-while True:
-    try:
-        number = int(input("Ingresa un valor: "))
-        print(15/number)
-        break
-    except ZeroDivisionError:
-        print("El valor ingresado no es valido...")
-    except:
-        print("Lo siento. Se me hace imposible realizar una division con 0...")
 
-#ValueError
-while True:
-    try:
-        number = int(input("Digita un valor: "))
-        print(7/number)
-        break
-    except ValueError:
-        print("Lo siento no detectamos el valor...")
-    except:
-        print("Advertencia: El valor digitado no es valida porfavor cambiarlo...")
+#Se escribe try para el intento de lo que queremos hacer
+try:
+    num=int(input("Escribe un numero: ")) #identacion
+    print("El resultado es", 1/num)
+#Si no puede aparecera el error
+except ZeroDivisionError:
+    print("Me queda muy dificil resolver una division con 0") #Lo que hace es evitar que aparecan una cantidad de texto que muestra el error
 
-#TypeError
+#ValueError: Este pasa cuando se digitan mal los numero en una operacion ejemplo que si en una division hay decimales o no
 
+#Se escirbe try para el intento de lo que deseamos que haga el usuario
+try:
+    num=int(input("porfavor ingresar un valor: ")) #La identacion de lo que se desea reaalizar
+    print("El resultado es", 1/num)
+#Si no puede aparecera el siguiente error
+except ValueError:
+    print("Lo siento. No puedo hacer esa operacion")
 
-#AttributeError
+#IndexError: Este se arroja cuando en una lista no se cumple con los requesitos dados
 
+#Se crea una lista con diferentes argumentos de manera ordenada
+list=[1,2,3,4,5,6]
 
-#SyntaxError
-
+#Se escribe try para el intento de lo que deseamos que haga el usuario
+try:
+    num=int(input("Ingrese un valor: ")) #El intento que se desea que el usuario ingrese colocando un numero de manera aleatoria
+    print(list[num]) #Se coloca la lista y se le añaden los numeros que puede ingresar
+except IndexError:
+    print("Lo siento no detecto ese numero en la lista")
